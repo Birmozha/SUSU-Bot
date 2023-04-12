bf88f68a240c1fad71a3334a86336191e35c7018
Birmozha <birmozha@gmail.com>
2023-04-13 00:03:40 +0500

    Added complaints

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
 import sqlite3
 import os
+import smtplib 
+from email.mime.multipart import MIMEMultipart                 
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import asyncio
+
 
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
@@ -10,18 +16,32 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import Text
 
 
+
+
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+TOKEN = os.environ.get('TOKEN')
+MAIL_BOX = os.environ.get('MAIL_BOX')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
-bot = Bot(os.environ.get('TOKEN'), parse_mode='HTML')
+bot = Bot(TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+class InfoStates(StatesGroup):
+    dialog = State()
+
+class ComplainStates(StatesGroup):
+    complain = State()
+    wait_text = State()
+    wait_photo = State()
+    wait_choose = State()    
 
 # ОБРАБОТЧИК КОМАНДЫ /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
+@dp.message_handler(commands=['start'], state=['*'])
+async def start(message: types.Message, state: FSMContext):
+    await state.finish()
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
         # ОТБОР ID ПЕРВОГО ЭЛЕМЕНТА ИЗ БАЗЫ ДАННЫХ
@@ -50,6 +70,7 @@ async def start(message: types.Message):
                 kbs.append(cur.execute(
                     """SELECT text FROM data WHERE id IS (?) """, (id)
                     ).fetchone())
+    
     # СОЗДАНИЕ КЛАВИАТУРЫ
     if ikbs:
         buttonsText = [button for button in ikbs] # СОЗДАНИЕ СПИСКА ТИПА [('Text', ID), ('Text', ID), ...]
@@ -70,15 +91,283 @@ async def start(message: types.Message):
         await message.answer(text=text)
 
 
-# ОБРАБОТЧИК INLINE-КНОПОК 
 @dp.callback_query_handler()
-async def dialog(callback: types.CallbackQuery, state: FSMContext):
+async def definePath(callback: types.CallbackQuery, state: FSMContext):
     await callback.answer()
+    if int(callback.data) == 2:
+        async with state.proxy() as st:
+            st['start'] = int(callback.data)
+        await InfoStates.dialog.set()
+        await callback.message.answer('Пожалуйста, используй кнопки', reply_markup=InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Хорошо', callback_data='continue')))
+    elif int(callback.data) == 17:
+        async with state.proxy() as st:
+            st['start'] = int(callback.data)
+        await ComplainStates.complain.set()
+        async with state.proxy() as st:
+            st['complain'] = []
+        await callback.message.answer('Пожалуйста, следуйте инструкциям', reply_markup=InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Хорошо', callback_data='continue')))
+
+@dp.message_handler(state=ComplainStates.wait_choose)
+async def waitText(message: types.Message, state: FSMContext):
+    async with state.proxy() as st:
+        pid = st['prev']
+    async with state.proxy() as st:
+        st['complain'].append(message.text)
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        # НАХОЖДЕНИЕ ID ПОЛУЧЕННОГО ТЕКСТОВОГО СООБЩЕНИЯ (ВОПРОСА)
+        pids = cur.execute(
+            """SELECT qid FROM tree WHERE pid is (?) """, (pid, )
+        ).fetchall()
+        for el in pids:
+            temp = cur.execute(
+                """SELECT data.id FROM data, tree WHERE data.text is (?) AND data.id IS (?) """, (message.text, el[0])
+            ).fetchone()
+            if temp:
+                temp = temp[0]
+                break
+        # ОТБОР ИЗ БАЗЫ ДАННЫХ ДОЧЕРНЕГО ID ЭЛЕМЕНТА К ID ТЕКСТОВОГО СООБЩЕНИЯ
+        qid, prop = cur.execute(
+            """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (temp, )
+            ).fetchall()[0]
+        prop = prop.split(', ')[1]
+        # КОНВЕРТАЦИЯ ID В ТЕКСТ ДЛЯ ОТПРАВКИ СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЮ
+        text = cur.execute(
+            """SELECT data.text FROM data, tree WHERE data.id IS (?) AND tree.pid IN (SELECT qid FROM tree WHERE pid is (?)) """, (qid, pid)
+            ).fetchone()[0]
+        # ОТБОР ДОЧЕРНИХ ЭЛЕМЕНТОВ (КНОПОК)
+        bid = cur.execute(
+            """SELECT qid FROM tree WHERE pid IS (?) AND properties LIKE '<button%' """, (qid, )
+            ).fetchall()
+        kbs = []
+        for id in bid:
+            kbs.append(cur.execute(
+                """SELECT text FROM data WHERE id IS (?) """, (id)
+                ).fetchone())
+    if prop == '<choosecat>':
+        await ComplainStates.wait_choose.set()
+    elif prop == '<waitphoto>':
+        await ComplainStates.wait_photo.set()
+    elif prop == '<waittext>':
+        await ComplainStates.wait_text.set()
+
+    
+    if kbs:
+        buttonsText = [button[0] for button in kbs] # СОЗДАНИЕ СПИСКА ТИПА ['Text', 'Text', ...]
+        # СОЗДАНИЕ КЛАВИАТУРЫ
+        kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True
+                                 ).add(*[KeyboardButton(text=text) for text in buttonsText]) # ДОБАВЛЕНИЕ КНОПОК
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ
+        await message.answer(text=text,
+                             reply_markup=kb) # ПЕРЕДАЧА КЛАВИТАУРЫ В TELEGRAM
+    else:
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ, ЕСЛИ КНОПОК НЕТ
+        await message.answer(text=text)
+    
+    async with state.proxy() as st:
+        st['prev'] = qid
+     
+
+@dp.message_handler(content_types=['any'], state=ComplainStates.wait_photo)
+async def waitPhoto(message: types.Message, state: FSMContext):
+    async with state.proxy() as st:
+        pid = st['prev']
+        cat = st['complain'][0]
+        
+    if message.photo:
+        await message.photo[-1].download(destination_file=f'photos/{cat}-{message.message_id}.jpg')
+        async with state.proxy() as st:
+            st['photo_path'] = f'photos/{cat}-{message.message_id}.jpg'
+            st['photo_name'] = f'{cat}-{message.message_id}.jpg'
+        with sqlite3.connect('data.db') as db:
+            cur = db.cursor()
+            next = cur.execute("""
+                        SELECT qid FROM tree WHERE pid is (?)
+                        """, (pid, )).fetchall()
+            if not next:
+                async with state.proxy() as st:
+                    t = st['complain']
+                    photo_path = st['photo_path']
+                    photo_name = st['photo_name']
+                addr_from = MAIL_BOX
+                addr_to = MAIL_BOX
+                msg = MIMEMultipart()
+                msg['From'] = addr_from
+                msg['To'] = addr_to
+                msg['Subject'] = f'Новая жалоба: {t[0]}'
+                body = (f'''--{t[1]}--''')
+                msg.attach(MIMEText(body, 'plain'))
+                # await asyncio.sleep(1)
+                with open(f'{photo_path}', 'rb') as fp:
+                    img = MIMEImage(fp.read())
+                    img.add_header('Content-Disposition', 'attachment', filename=f"{photo_name}")
+                    msg.attach(img)
+                smtpObj = smtplib.SMTP('smtp.mail.ru')
+                smtpObj.starttls()
+                smtpObj.login(addr_from, MAIL_PASSWORD)
+                smtpObj.send_message(msg)
+                smtpObj.quit()
+                await state.finish()
+                return await message.answer(text='Спасибо, ваша жалоба сохранена')
+        with sqlite3.connect('data.db') as db:
+            cur = db.cursor()
+            qid, prop = cur.execute(
+                """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (pid, )
+                ).fetchall()[0]
+            prop = prop.split(', ')[1]
+            # КОНВЕРТАЦИЯ ID В ТЕКСТ ДЛЯ ОТПРАВКИ СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЮ
+            text = cur.execute("""
+                            SELECT text FROM data WHERE id IS (?)
+                            """, (qid, )).fetchone()[0]
+            kbs = []
+            if prop == '<choosecat>':
+                bid = cur.execute(
+                    """SELECT qid FROM tree WHERE pid IS (?) AND properties LIKE '<button%' """, (qid, )
+                    ).fetchall()
+                for id in bid:
+                    kbs.append(cur.execute(
+                        """SELECT text FROM data WHERE id IS (?) """, (id)
+                        ).fetchone())
+        if prop == '<choosecat>':
+            await ComplainStates.wait_choose.set()
+        elif prop == '<waitphoto>':
+            await ComplainStates.wait_photo.set()
+        elif prop == '<waittext>':
+            await ComplainStates.wait_text.set()
+        
+        if kbs:
+            buttonsText = [button[0] for button in kbs] # СОЗДАНИЕ СПИСКА ТИПА ['Text', 'Text', ...]
+            # СОЗДАНИЕ КЛАВИАТУРЫ
+            kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True
+                                    ).add(*[KeyboardButton(text=text) for text in buttonsText]) # ДОБАВЛЕНИЕ КНОПОК
+            # ОТПРАВКА СООБЩЕНИЯ БОТОМ
+            await message.answer(text=text,
+                                reply_markup=kb) # ПЕРЕДАЧА КЛАВИТАУРЫ В TELEGRAM
+        else:
+            # ОТПРАВКА СООБЩЕНИЯ БОТОМ, ЕСЛИ КНОПОК НЕТ
+            await message.answer(text=text)
+        
+    else:
+        await message.reply(text='Пожалуйста, пришлите фото')
+
+@dp.message_handler(state=ComplainStates.wait_text)
+async def waitText(message: types.Message, state: FSMContext):
+    async with state.proxy() as st:
+        pid = st['prev']
+        st['complain'].append(message.text)
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        next = cur.execute("""
+                        SELECT qid FROM tree WHERE pid is (?)
+                        """, (pid, )).fetchall()
+        if not next:
+            await state.finish()
+            return await message.answer(text='Спасибо, ваша жалоба сохранена')
+        qid, prop = cur.execute(
+            """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (pid, )
+            ).fetchall()[0]
+        prop = prop.split(', ')[1]
+        # КОНВЕРТАЦИЯ ID В ТЕКСТ ДЛЯ ОТПРАВКИ СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЮ
+        text = cur.execute("""
+                           SELECT text FROM data WHERE id IS (?)
+                           """, (qid, )).fetchone()[0]
+        kbs = []
+        if prop == '<choosecat>':
+            bid = cur.execute(
+                """SELECT qid FROM tree WHERE pid IS (?) AND properties LIKE '<button%' """, (qid, )
+                ).fetchall()
+            for id in bid:
+                kbs.append(cur.execute(
+                    """SELECT text FROM data WHERE id IS (?) """, (id)
+                    ).fetchone())
+    if prop == '<choosecat>':
+        await ComplainStates.wait_choose.set()
+    elif prop == '<waitphoto>':
+        await ComplainStates.wait_photo.set()
+    elif prop == '<waittext>':
+        await ComplainStates.wait_text.set()
+    
+    if kbs:
+        buttonsText = [button[0] for button in kbs] # СОЗДАНИЕ СПИСКА ТИПА ['Text', 'Text', ...]
+        # СОЗДАНИЕ КЛАВИАТУРЫ
+        kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True
+                                 ).add(*[KeyboardButton(text=text) for text in buttonsText]) # ДОБАВЛЕНИЕ КНОПОК
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ
+        await message.answer(text=text,
+                             reply_markup=kb) # ПЕРЕДАЧА КЛАВИТАУРЫ В TELEGRAM
+    else:
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ, ЕСЛИ КНОПОК НЕТ
+        await message.answer(text=text)
+    
+    async with state.proxy() as st:
+        st['prev'] = qid
+     
+
+
+@dp.callback_query_handler(state=ComplainStates.complain)
+async def complainStart(callback: types.CallbackQuery, state: FSMContext):
+    await callback.answer()
+    if callback.data == 'continue':
+        async with state.proxy() as st:
+                search_id = st['start']
+    else:
+        search_id = callback.data
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
         # ОТБОР  ИЗ БАЗЫ ДАННЫХ ДОЧЕРНЕГО ID ЭЛЕМЕНТА К ПОЛУЧЕННОМУ ID ОТ CALLBACK-ЗАПРОСА
         qid, prop = cur.execute(
-            """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (callback.data, )
+            """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (search_id, )
+            ).fetchall()[0]
+        prop = prop.split(', ')[1]
+        # КОНВЕРТАЦИЯ ID В ТЕКСТ ДЛЯ ОТПРАВКИ СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЮ
+        text = cur.execute(
+            """SELECT text FROM data WHERE id IS (?) """, (qid, )
+            ).fetchone()[0]
+        # ОТБОР ДОЧЕРНИХ ЭЛЕМЕНТОВ (КНОПОК)
+        bid = cur.execute(
+            """SELECT qid FROM tree WHERE pid IS (?) AND properties LIKE '<button%' """, (qid, )
+            ).fetchall()
+        kbs = []
+        for id in bid:
+            # СОЗДАНИЕ СПИСКА ИНЛАЙН-КНОПОК
+            kbs.append(cur.execute(
+                """SELECT text FROM data WHERE id IS (?) """, (id)
+                ).fetchone())
+    if prop == '<choosecat>':
+        await ComplainStates.wait_choose.set()
+    elif prop == '<waitphoto>':
+        await ComplainStates.wait_photo.set()
+    elif prop == '<waittext>':
+        await ComplainStates.wait_text.set()
+
+    if kbs:
+        buttonsText = [button[0] for button in kbs] # СОЗДАНИЕ СПИСКА ТИПА ['Text', 'Text', ...]
+        # СОЗДАНИЕ КЛАВИАТУРЫ
+        kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True
+                                 ).add(*[KeyboardButton(text=text) for text in buttonsText]) # ДОБАВЛЕНИЕ КНОПОК
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ
+        await callback.message.answer(text=text,
+                                      reply_markup=kb) # ПЕРЕДАЧА КЛАВИТАУРЫ В TELEGRAM
+    else:
+        # ОТПРАВКА СООБЩЕНИЯ БОТОМ, ЕСЛИ КНОПОК НЕТ
+        await callback.message.answer(text=text)
+    async with state.proxy() as st:
+        st['prev'] = qid
+
+# ОБРАБОТЧИК INLINE-КНОПОК 
+@dp.callback_query_handler(state=InfoStates.dialog)
+async def dialog(callback: types.CallbackQuery, state: FSMContext):
+    await callback.answer()
+    if callback.data == 'continue':
+        async with state.proxy() as db:
+                search_id = db['start']
+    else:
+        search_id = callback.data
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        # ОТБОР  ИЗ БАЗЫ ДАННЫХ ДОЧЕРНЕГО ID ЭЛЕМЕНТА К ПОЛУЧЕННОМУ ID ОТ CALLBACK-ЗАПРОСА
+        qid, prop = cur.execute(
+            """SELECT qid, properties FROM tree WHERE pid IS (?) and properties LIKE '<text>%' """, (search_id, )
             ).fetchall()[0]
         prop = prop.split(', ')[1]
         # КОНВЕРТАЦИЯ ID В ТЕКСТ ДЛЯ ОТПРАВКИ СООБЩЕНИЯ ПОЛЬЗОВАТЕЛЮ
@@ -103,7 +392,7 @@ async def dialog(callback: types.CallbackQuery, state: FSMContext):
                     """SELECT text FROM data WHERE id IS (?) """, (id)
                     ).fetchone())
     
-    
+        
     if ikbs:
         buttonsText = [button for button in ikbs] # СОЗДАНИЕ СПИСКА ТИПА [('Text', ID), ('Text', ID), ...]
         # СОЗДАНИЕ КЛАВИАТУРЫ
@@ -128,11 +417,10 @@ async def dialog(callback: types.CallbackQuery, state: FSMContext):
         st['prev'] = qid
 
 # ОБРАБОТЧИК ТЕКСТОВОГО СООБЩЕНИЯ
-@dp.message_handler()
+@dp.message_handler(state=InfoStates.dialog)
 async def dialog(message: types.Message, state: FSMContext):
     async with state.proxy() as st:
         pid = st['prev']
-        # print(pid)
 
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
--- main.py
+++ main.py
@@ -49,19 +49,16 @@ def login():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return render_template('login.html')
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
-@app.route('/tree')
+@app.route('/bot-flow')
 @login_required
-def tree():
+def show_flow():
     tree = []
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
@@ -81,9 +78,41 @@ def tree():
             temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
             temp = (temp[0], tuple(temp[1].split(', ')))
             tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+    return render_template('botFlow.html', tree=tree)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template('index.html')
+
+@app.route('/info-tree')
+@login_required
+def info_tree():
+    tree = []
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[0][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
     return render_template('tree.html', tree=tree)
 
-@app.route('/tree/<int:id>/change', methods=['POST', 'GET'])
+@app.route('/info-tree/<int:id>/change', methods=['POST', 'GET'])
+@login_required
 def changeLeaf(id):
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
@@ -118,12 +147,15 @@ def changeLeaf(id):
                                 WHERE id IS (?) ;
                                 """, (property, id))
             
-        return redirect('/tree')
+        return redirect('/info-tree')
     
     return render_template('changeLeaf.html', data=data, properties=properties, id=id)
 
 
-@app.route('/delete/<int:id>')
+
+
+@app.route('/info-tree/delete/<int:id>')
+@login_required
 def deleteLeaf(id):
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
@@ -136,31 +168,32 @@ def deleteLeaf(id):
             print(el)
             cur.execute("""DELETE FROM data WHERE id is (?) """, (el))
         
-    return redirect('/tree')
+    return redirect('/info-tree')
 
 
-@app.route('/tree/add', methods=['POST', 'GET'])
+@app.route('/info-tree/add', methods=['POST', 'GET'])
+@login_required
 def addLeaf():
     tree = []
     with sqlite3.connect('data.db') as db:
         cur = db.cursor()
-        nulls = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchall()
-        for null in nulls:
-            level_id = cur.execute("""WITH RECURSIVE
-                            cte(qid, level) AS (
-                                VALUES((?), 0)
-                                UNION ALL
-                                SELECT tree.qid, cte.level+1
-                                FROM tree JOIN cte ON tree.pid = cte.qid
-                                ORDER BY 2 DESC)
-                                SELECT level, qid FROM cte""", (null[0], )).fetchall()
-            
-            level_id = [el for el in level_id]
-            tree = []
-            for el in level_id:
-                temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
-                temp = (temp[0], tuple(temp[1].split(', ')))
-                tree.append((el[0]+1, temp[0], el[1], temp[1]))
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[0][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
         properties = list(map(lambda x: x[0], set(cur.execute("""SELECT properties FROM tree""").fetchall())))
         
         
@@ -169,11 +202,11 @@ def addLeaf():
         property = request.form['button-type']
         pid = request.form['pid']
         if not text:
-            return redirect('/tree/add')
+            return redirect('/info-tree/add')
         elif property == '--Выберите вид элемента--':
-            return redirect('/tree/add')
+            return redirect('/info-tree/add')
         elif pid == '--Выберите элемент-родитель--':
-            return redirect('/tree/add')
+            return redirect('/info-tree/add')
 
         with sqlite3.connect('data.db') as db:
             cur = db.cursor()
@@ -184,10 +217,83 @@ def addLeaf():
                         VALUES ((?))
                         """, (text, ))
             
-        return redirect('/tree/add')
-        
-        
+        return redirect('/info-tree/add')
     return render_template('addLeaf.html', tree=tree, properties=properties)
+
+
+@app.route('/complain-tree')
+@login_required
+def complain_tree():
+    tree = []
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[1][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)    
+    return render_template('complainTree.html',tree=tree)
+
+@app.route('/complain-tree/add', methods=['POST', 'GET'])
+@login_required
+def addComplainLeaf():
+    tree = []
+    with sqlite3.connect('data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[1][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+        properties = list(map(lambda x: x[0], set(cur.execute("""SELECT properties FROM tree""").fetchall())))
+        
+        
+    if request.method == 'POST':
+        text = request.form['text']
+        property = request.form['block-type']
+        pid = request.form['pid']
+        if not text:
+            return redirect('/complain-tree/add')
+        elif property == '--Выберите вид элемента--':
+            return redirect('/complain-tree/add')
+        elif pid == '--Выберите элемент-родитель--':
+            return redirect('/complain-tree/add')
+
+        with sqlite3.connect('data.db') as db:
+            cur = db.cursor()
+            cur.execute("""INSERT INTO tree (pid, properties)
+                        VALUES ((?), (?))
+                        """, (pid, property))
+            cur.execute("""INSERT INTO data (text)
+                        VALUES ((?))
+                        """, (text, ))
+            
+        return redirect('/complain-tree/add')
+    return render_template('addComplainLeaf.html', tree=tree, properties=properties)
 
 
 if __name__ == '__main__':
